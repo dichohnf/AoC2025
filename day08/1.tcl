a899c9166cab68d16cfbd936a37af2e7230e151d
dichohnf <mattedicio@gmail.com>
2025-12-29 10:50:08 +0100

    day08 complete

--- day08/1.tcl
+++ day08/1.tcl
@@ -1,63 +1,82 @@
 #!/usr/bin/tclsh
 
-set fp [open "test" r]
+set input "input"
+set iterations 1000
+
+set fp [open $input r]
 set file_text [read $fp]
 close $fp
 set lines [split $file_text "\n"]
 
 set points {}
-foreach line $lines {
-    lappend points [split $line ","]
+set point_circuit {}
+for {set idx 0} {$idx < [llength $lines]} {incr idx} {
+  set line [lindex $lines $idx]
+  lappend points [split $line ","]
+  dict set point_circuit [split $line ","] $idx
 }
 
-set circuits {}
-set connected {}
 set distances {}
 
-foreach start_point $points {
-    if {[lsearch -exact $connected $start_point]>=0} {
-        continue
-    }
+for {set i 0} {$i < [llength $points]} {incr i} {
+  set first [lindex $points $i]
+  set fx [lindex $first 0]
+  set fy [lindex $first 1]
+  set fz [lindex $first 2]
+  for {set j [expr $i + 1]} {$j < [llength $points]} {incr j} {
+    set second [lindex $points $j]
+    set sx [lindex $second 0]
+    set sy [lindex $second 1]
+    set sz [lindex $second 2]
 
-    set start_point_idx [lsearch -exact $points $start_point]
-    set point_distances {}
-    foreach end_point [lreplace $points $start_point_idx $start_point_idx] {
-        if {[dict exists $distances "$end_point,$start_point"]} {
-            set distance [dict get $distances "$end_point,$start_point"]
-        } else {
-            set s_x [lindex $start_point 0]
-            set s_y [lindex $start_point 1]
-            set s_z [lindex $start_point 2]
-            set e_x [lindex $end_point 0]
-            set e_y [lindex $end_point 1]
-            set e_z [lindex $end_point 2]
-            set distance [expr {($s_x-$e_x)**2 + ($s_y-$e_y)**2 + ($s_z-$e_z)**2}]
-        }
-        lappend point_distances [list $end_point $distance]
-        dict set distances "$start_point,$end_point" $distance
-    }
-    set point_distances [lsort -integer -index 1 $point_distances]
-    set connect_to [lindex [lindex $point_distances 0] 0]
-    set added_to_existing false
-    foreach circuit $circuits {
-        if {[lsearch -exact $circuit $connect_to]>=0} {
-            set circuit_idx [lsearch $circuits $circuit]
-            lappend circuit $start_point
-            set circuits [lreplace $circuits $circuit_idx $circuit_idx $circuit]
-            set added_to_existing true
-            break
-        }
-    }
-    if {!$added_to_existing} {
-        lappend circuits [list $start_point $connect_to]
-    }
-    if {$start_point ni $connected} {
-        lappend connected $start_point
-    }
-    if {$connect_to ni $connected} {
-        lappend connected $connect_to
-    }
+    set distance_squared [expr {($fx-$sx)**2 + ($fy-$sy)**2 + ($fz-$sz)**2}]
+    lappend distances [list $first $second $distance_squared]
+  }
 }
+
+set distances [lsort -integer -index 2 $distances]
+
+set iteration 0
+set circuits {}
+
+while {$iteration < $iterations} {
+  set distance [lindex $distances $iteration]
+  set first [lindex $distance 0]
+  set second [lindex $distance 1]
+
+  set circuit_first_idx -1
+  set circuit_second_idx -1
+  for {set circuit_idx 0} {$circuit_idx < [llength $circuits]} {incr circuit_idx} {
+    set circuit [lindex $circuits $circuit_idx]
+    if {[lsearch $circuit $first] > -1} {
+        set circuit_first_idx $circuit_idx
+    }
+    if {[lsearch $circuit $second] > -1} {
+      set circuit_second_idx $circuit_idx
+    }
+  }
+
+  if {$circuit_first_idx == -1 && $circuit_second_idx == -1} {
+    lappend circuits [list $first $second]
+  } elseif {$circuit_first_idx == -1 && $circuit_second_idx != -1} {
+    set circuit [lindex $circuits $circuit_second_idx]
+    lappend circuit $first
+    set circuits [lreplace $circuits $circuit_second_idx $circuit_second_idx $circuit]
+  } elseif {$circuit_first_idx != -1 && $circuit_second_idx == -1} {
+    set circuit [lindex $circuits $circuit_first_idx]
+    lappend circuit $second
+    set circuits [lreplace $circuits $circuit_first_idx $circuit_first_idx $circuit]
+  } elseif {$circuit_first_idx != $circuit_second_idx} {
+    set circuit_first [lindex $circuits $circuit_first_idx]
+    set circuit_second [lindex $circuits $circuit_second_idx]
+    set circuit [concat $circuit_first $circuit_second]
+    set circuits [lreplace $circuits $circuit_first_idx $circuit_first_idx $circuit]
+    set circuits [lreplace $circuits $circuit_second_idx $circuit_second_idx]
+  }
+
+  incr iteration
+}
+
 
 proc compare_by_length {a b} {
     return [expr {[llength $b] - [llength $a]}]
@@ -69,7 +88,4 @@ set first_length [llength [lindex $circuits 0]]
 set second_length [llength [lindex $circuits 1]]
 set third_length [llength [lindex $circuits 2]]
 
-puts [format "1- %s" [lindex $circuits 0]]
-puts [format "2- %s" [lindex $circuits 1]]
-puts [format "3- %s" [lindex $circuits 2]]
 puts [format "%d * %d * %d = %d" $first_length $second_length $third_length [expr {$first_length * $second_length * $third_length}]]
--- day08/2.tcl
+++ day08/2.tcl
@@ -1,39 +1,98 @@
 #!/usr/bin/tclsh
 
-proc new_timeline {start_row col} {
-    upvar lines lines cache cache
-    puts [format "%d - %d" $start_row $col]
-    if {[dict exists $cache "$start_row,$col"]} {
-        return [dict get $cache "$start_row,$col"]
-    }
-    for {set row [expr $start_row + 1]} {$row<[llength $lines]} {incr row} {
-        set line [lindex $lines $row]
-        set char [string index $line $col]
-        if {$char eq "^"} {
-            set tl_prev [new_timeline $row [expr $col - 1]]
-            dict set cache "$row,[expr $col - 1]" $tl_prev
-            set tl_next [new_timeline $row [expr $col + 1]]
-            dict set cache "$row,[expr $col + 1]" $tl_next
-            return [expr $tl_prev + $tl_next]
-        }
-    }
-    return 1
-}
+set input "input"
 
-set fp [open "input" r]
+set fp [open $input r]
 set file_text [read $fp]
 close $fp
 set lines [split $file_text "\n"]
 
-set timelines 0
-set cache {}
-set start_line [lindex $lines 0]
-for {set col 0} {$col<[string length $start_line]} {incr col} {
-    set start [string index $start_line $col]
-    if {$start eq "S"} {
-        set timelines [new_timeline 0 $col]
-        break
-    }
+set points {}
+set point_circuit {}
+for {set idx 0} {$idx < [llength $lines]} {incr idx} {
+  set line [lindex $lines $idx]
+  lappend points [split $line ","]
+  dict set point_circuit [split $line ","] $idx
 }
 
-puts $timelines
+set distances {}
+
+for {set i 0} {$i < [llength $points]} {incr i} {
+  set first [lindex $points $i]
+  set fx [lindex $first 0]
+  set fy [lindex $first 1]
+  set fz [lindex $first 2]
+  for {set j [expr $i + 1]} {$j < [llength $points]} {incr j} {
+    set second [lindex $points $j]
+    set sx [lindex $second 0]
+    set sy [lindex $second 1]
+    set sz [lindex $second 2]
+
+    set distance_squared [expr {($fx-$sx)**2 + ($fy-$sy)**2 + ($fz-$sz)**2}]
+    lappend distances [list $first $second $distance_squared]
+  }
+}
+
+set distances [lsort -integer -index 2 $distances]
+
+set iteration 0
+set circuits {}
+
+puts [format "Points: %d" [llength $points]]
+
+while {[llength [lindex $circuits 0]] != [llength $points]} {
+  puts [format "First circuit length: %d" [llength [lindex $circuits 0]]]
+  set distance [lindex $distances $iteration]
+  set first [lindex $distance 0]
+  set second [lindex $distance 1]
+
+  set circuit_first_idx -1
+  set circuit_second_idx -1
+  for {set circuit_idx 0} {$circuit_idx < [llength $circuits]} {incr circuit_idx} {
+    set circuit [lindex $circuits $circuit_idx]
+    if {[lsearch $circuit $first] > -1} {
+        set circuit_first_idx $circuit_idx
+    }
+    if {[lsearch $circuit $second] > -1} {
+      set circuit_second_idx $circuit_idx
+    }
+  }
+
+  if {$circuit_first_idx == -1 && $circuit_second_idx == -1} {
+    lappend circuits [list $first $second]
+  } elseif {$circuit_first_idx == -1 && $circuit_second_idx != -1} {
+    set circuit [lindex $circuits $circuit_second_idx]
+    lappend circuit $first
+    set circuits [lreplace $circuits $circuit_second_idx $circuit_second_idx $circuit]
+  } elseif {$circuit_first_idx != -1 && $circuit_second_idx == -1} {
+    set circuit [lindex $circuits $circuit_first_idx]
+    lappend circuit $second
+    set circuits [lreplace $circuits $circuit_first_idx $circuit_first_idx $circuit]
+  } elseif {$circuit_first_idx != $circuit_second_idx} {
+    set circuit_first [lindex $circuits $circuit_first_idx]
+    set circuit_second [lindex $circuits $circuit_second_idx]
+    set circuit [concat $circuit_first $circuit_second]
+    set circuits [lreplace $circuits $circuit_first_idx $circuit_first_idx $circuit]
+    set circuits [lreplace $circuits $circuit_second_idx $circuit_second_idx]
+  }
+
+  incr iteration
+}
+
+set fx [lindex $first 0]
+set sx [lindex $second 0]
+
+puts [format "Mult. of X coordinates: %d" [expr $fx * $sx]]
+
+
+#proc compare_by_length {a b} {
+#    return [expr {[llength $b] - [llength $a]}]
+#}
+#
+#set circuits [lsort -command compare_by_length $circuits]
+#
+#set first_length [llength [lindex $circuits 0]]
+#set second_length [llength [lindex $circuits 1]]
+#set third_length [llength [lindex $circuits 2]]
+#
+#puts [format "%d * %d * %d = %d" $first_length $second_length $third_length [expr {$first_length * $second_length * $third_length}]]
